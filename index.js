116bd0feea3a42f6b17a77dd3b01bc8993fe053d
carlota-taveira <114014431+carlota-taveira@users.noreply.github.com>
2022-10-27 16:54:23 +0100

    1st commit

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 const mongoose = require('mongoose');
-
-// Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
 
+const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 //Method 1 : Using Async Await
 
+const pasta = { title: 'Pasta Bolognese', level: 'Easy Peasy', ingredients: ['pasta', 'meat', 'tomato sauce'], cuisine: 'italian', dishType: 'main_course', duration: 20, creator: 'Carlota' };
+
+
 const manageRecipes = async () => {
   try {
-    // Connection to the database "recipe-app"
     const dbConnection = await mongoose.connect(MONGODB_URI);
     console.log(`Connected to the database: "${dbConnection.connection.name}"`);
 
     // Before adding any recipes to the database, let's remove all existing ones
     await Recipe.deleteMany();
+    await Recipe.create(pasta);
 
-    // Run your code here, after you have insured that the connection was made
+    let multipleRecipes = await Recipe.insertMany(data);
+    multipleRecipes.forEach(recipes => console.log(recipes.title))
+
+    let rec = await Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+    console.log(rec);
+    console.log("Done")
+
+    await Recipe.deleteOne({ title: 'Carrot Cake' })
+    console.log("Done");
+
+    mongoose.disconnect();
+
   } catch (error) {
+
     console.log(error);
   }
 };
-
 manageRecipes();
+
+
+
+
+
+
+
+
 
 //Method 2: Using .then() method
 //If you want to use this method uncomment the code below:
